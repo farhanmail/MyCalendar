fa2992cb3f3bad805b3946a3381f9b412c355364
farhanmail <maill.farhanismail@gmail.com>
2021-12-23 11:13:20 +0700

    add full calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar from './Components/calendar';
 import Header from './Components/header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Calendar/>
     </div>
   );
 }
---
+++ src/Components/calendar.js
@@ -0,0 +1,28 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import React from 'react';
+
+
+function Calendar () {
+    return (
+        <>
+            <div>
+                <div className='calender' style={{marginLeft:'10%', marginRight:'10%', height: '90vh'}}>
+                    <FullCalendar
+                        plugins={[ dayGridPlugin ]}
+                        initialView="dayGridMonth"
+                        weekends={false}
+                        events={[
+                            { title: 'event 1', date: '2019-04-01' },
+                            { title: 'event 2', date: '2019-04-02' }
+                        ]}
+                    />
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Calendar;
